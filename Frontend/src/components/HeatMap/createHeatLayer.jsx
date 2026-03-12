3a86c21ace1841747aead79f3c68c67e591d9908
rahul122703 <killbusyness@gmail.com>
2026-03-13 01:05:03 +0530

    normalize the temperature values

--- Frontend/src/components/HeatMap/createHeatLayer.jsx
+++ Frontend/src/components/HeatMap/createHeatLayer.jsx
@@ -5,15 +5,21 @@ import Point from "ol/geom/Point";
 import { fromLonLat } from "ol/proj";
 
 export default function createHeatLayer(temperatureData) {
+  const MIN_TEMP = 30;
+  const MAX_TEMP = 40;
+
   const heatFeatures = temperatureData.temperature.map((p) => {
+    // normalize temp between 0 and 1
+    let weight = (p.value - MIN_TEMP) / (MAX_TEMP - MIN_TEMP);
+
+    // clamp value
+    weight = Math.max(0, Math.min(1, weight));
 
     const feature = new Feature({
       geometry: new Point(fromLonLat([p.lng, p.lat])),
     });
 
-    // scale temperature to heat weight
-    feature.set("weight", p.value / 50);
-
+    feature.set("weight", Math.pow(weight, 2));
     return feature;
   });
 
