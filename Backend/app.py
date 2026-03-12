a63adff9d85c187a8c57786c22c5ed376588c603
rahul122703 <killbusyness@gmail.com>
2026-03-12 18:12:01 +0530

    adding region division

--- Backend/app.py
+++ Backend/app.py
@@ -1,26 +1,70 @@
 from flask import Flask, jsonify
+from tensorflow.keras.models import load_model
 import numpy as np
-from keras.models import load_model
+import io
 
 app = Flask(__name__)
 
-pre_monsoon_model = load_model("k2_west_pre_monsoon.h5", compile=False)
+# load model once when server starts
+model = load_model("k2_west_pre_monsoon.h5", compile=False)
 
-@app.route("/")
-def home():
-    return {"message": "API running"}
 
-@app.route("/predict/pre_monsoon")
-def predict_pre_monsoon():
+@app.route("/", methods=["GET"])
+def index():
+    return jsonify({
+        "message": "API is running",
+        "available_endpoints": [
+            "/summary",
+            "/west-premonsoon"
+        ]
+    })
 
-    sample = np.zeros((1,45,3))   # correct shape
-    print("sample")
-    print(sample)
-    prediction = pre_monsoon_model.predict(sample)
+
+@app.route("/summary", methods=["GET"])
+def model_summary():
+    stream = io.StringIO()
+
+    model.summary()
+    model.summary(print_fn=lambda x: stream.write(x + "\n"))
+    summary_text = stream.getvalue()
 
     return jsonify({
-        "prediction": prediction.tolist()
+        "input_shape": model.input_shape,
+        "output_shape": model.output_shape,
+        "summary": summary_text
     })
+
+
+@app.route("/west-premonsoon", methods=["GET"])
+def west_premonsoon_prediction():
+
+    # internal dummy input (45 days × 3 features)
+    sample_input = np.random.rand(45, 3)
+
+    sample_input = sample_input.reshape(1, 45, 3)
+
+    prediction = model.predict(sample_input)
+
+    return jsonify({
+        "forecast_next_7_days": prediction.tolist()[0]
+    })
+
+
+# handle all unknown routes
+@app.errorhandler(404)
+def route_not_found(e):
+    return jsonify({
+        "error": {
+            "code": 404,
+            "message": "Endpoint does not exist",
+            "available_endpoints": [
+                "/",
+                "/summary",
+                "/west-premonsoon"
+            ]
+        }
+    }), 
+
 
 if __name__ == "__main__":
     app.run(debug=True)
